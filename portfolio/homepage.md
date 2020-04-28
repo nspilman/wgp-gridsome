d2ac8a5fe7a5055f3bbe868a63893bdf752fc79f
Nate Spilman <nate.spilman@gmail.com>
2020-04-27 22:31:56 -0700

    updating homepage carosel to use local images. added portfolio to yml

--- portfolio/homepage.md
+++ portfolio/homepage.md
@@ -2,14 +2,14 @@
 title: homepage
 ---
 
-![homepage photos](uploads/homepage/DSC_4096.jpg
-![homepage photos](uploads/homepage/DSC_4876.jpg
-![homepage photos](uploads/homepage/BoysAlbumCoverSunflowerscopy2.jpg
-![homepage photos](uploads/homepage/DSC_0353-2.jpg
-![homepage photos](uploads/homepage/DSC_8440-2.jpg
-![homepage photos](uploads/homepage/DSC_5055.jpg
-![homepage photos](uploads/homepage/DSC_5694.jpg
-![homepage photos](uploads/homepage/DSC_2926.jpg
-![homepage photos](uploads/homepage/EmmyWiedemann.jpg
-![homepage photos](uploads/homepage/DSC_8891.jpg
-![homepage photos](uploads/homepage/DSC_6302.jpg
+![homepage photos](../uploads/homepage/DSC_4096.jpg)
+![homepage photos](../uploads/homepage/DSC_4876.jpg)
+![homepage photos](../uploads/homepage/BoysAlbumCoverSunflowerscopy2.jpg)
+![homepage photos](../uploads/homepage/DSC_0353-2.jpg)
+![homepage photos](../uploads/homepage/DSC_8440-2.jpg)
+![homepage photos](../uploads/homepage/DSC_5055.jpg)
+![homepage photos](../uploads/homepage/DSC_5694.jpg)
+![homepage photos](../uploads/homepage/DSC_2926.jpg)
+![homepage photos](../uploads/homepage/EmmyWiedemann.jpg)
+![homepage photos](../uploads/homepage/DSC_8891.jpg)
+![homepage photos](../uploads/homepage/DSC_6302.jpg)
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,14 +16,7 @@
                 <h2 class="h2">Wine Guy Photography</h2>
                 <p>Professional &amp; Affordable</p>
               </div>
-              <div class="ts-item-background">
-                <g-image v-if="isLoading" src="../assets/images/Cube-1s-200px.svg" alt="loading"/>
-                <g-image
-                  v-if="!isLoading"
-                  :src="currentPhoto"
-                  alt="background"
-                  class="img-fluid header-photo"
-                />
+              <div class="ts-item-background" v-html="currentPhoto">
                 <div class="bg-overlay"></div>
               </div>
             </div>
@@ -34,6 +27,20 @@
   </header>
 </template>
 
+<static-query>
+query Content {
+  images: allPortfolio {
+    edges {
+      node {
+        id
+        title
+        content
+      }
+    }
+  }
+}
+</static-query>
+
 <script>
 import Menu from './Navbar.vue';
 import axios from 'axios';
@@ -41,7 +48,6 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      homepagePhotos: [],
       currentPhotoIndex: 0,
       autoplay_bool: true,
       isLoading: true,
@@ -55,29 +61,11 @@ export default {
     currentPhoto() {
       return this.homepagePhotos[this.currentPhotoIndex];
     },
+    homepagePhotos(){
+      return this.$static.images.edges.map(edge => edge.node).filter(edge => edge.title == 'homepage')[0].content.replace('<p>','').replace('</p>','').split(/\n/ig);
+    }
   },
   methods: {
-    async getPhotosFromAws(portfolioCategory) {
-      const url = `https://py5e37ug41.execute-api.us-east-1.amazonaws.com/default/getPhotosByName?category=${portfolioCategory}`;
-      const resp = await axios.get(url);
-      const json = await resp.data;
-      for (let i = 1; i < (json.length); i += 1) {
-        this.homepagePhotos.push(json[i]);
-      }
-      this.loadPhotos();
-    },
-    loadPhotos() {
-      this.homepagePhotos.forEach((photo) => {
-        const img = new Image();
-        img.src = photo;
-        img.onload = () => {
-          this.imageLoaded = this.imageLoaded + 1;
-          if (this.imageLoaded > 6) {
-            this.isLoading = false;
-          }
-        };
-      }, this.autoplay());
-    },
     nextPhoto() {
       if (this.currentPhotoIndex < this.homepagePhotos.length - 1) {
         this.currentPhotoIndex = this.currentPhotoIndex + 1;
@@ -92,7 +80,7 @@ export default {
     },
   },
   mounted() {
-    this.getPhotosFromAws('photos-homepage-slider');
+    this.autoplay()
     document.addEventListener('contextmenu', (e) => {
       e.preventDefault();
       const copyright = document.querySelector('#copyright');
@@ -108,5 +96,10 @@ export default {
 <style>
 .header-photo{
    max-height: 100vh;
+}
+
+.ts-item-background img{
+max-height: 100vh;
+width:auto;
 }
 </style>
